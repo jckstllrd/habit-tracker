5d16dbdf66db7329eb1691eb9071c33b61825b4e
jckstllrd <jackstallard17@gmail.com>
2026-03-08 20:43:26 +0000

    feat: display the current habits to the dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router";
 export default function App() {
+  const [habits, setHabits] = useState([
+    {
+      id: crypto.randomUUID(),
+      name: "no scrolling",
+      completed_today: false,
+    },
+  ]);
 
+  function addHabit(habit) {
+    setHabits((prev) => [...prev, habit]);
+  }
   return (
     <>
       <nav>
@@ -9,16 +20,17 @@ export default function App() {
             <Link to="/">Dashboard</Link>
           </li>
           <li>
-            <Link to="add">
-              Add
-            </Link>
+            <Link to="add">Add Habit</Link>
           </li>
+
           <li>
             <Link to="settings">Settings</Link>
           </li>
         </ul>
       </nav>
-      <div className="app-container"><Outlet /></div>
+      <div className="app-container">
+        <Outlet context={[habits, addHabit]} />
+      </div>
     </>
   );
 }
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,17 @@
-
 import "../styles/Dashboard.css";
-import { Link } from "react-router";
+import { Link, useOutletContext } from "react-router";
 
 export default function Dashboard() {
+  const [habits] = useOutletContext();
+  
   return (
     <div>
       <h1>dashboard view</h1>
-      
+      <ul>
+        {habits.map((habit) => {
+          return <li key={habit.id}>{habit.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
