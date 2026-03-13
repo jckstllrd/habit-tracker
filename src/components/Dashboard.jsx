738e4a469d7fd452f97a810986770332683cbc65
jckstllrd <jackstallard17@gmail.com>
2026-03-13 22:41:42 +0000

    style: add styling to the new dashboard view

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,18 @@
 import { useOutletContext } from "react-router";
-import "../styles/Dashboard.css";
+import Habit from "./Habit.jsx";
+import styles from "../styles/Dashboard.module.css";
 
 export default function Dashboard() {
   const [habits, addHabit] = useOutletContext();
-
-  return <div className="dashboard-container"></div>;
+  let month = "may";
+  return (
+    <div className={styles.dashboardContainer}>
+      <h1 className={styles.month}>{month}</h1>
+      <div className={styles.habitGrid}>
+        {habits.map((habit, index) => {
+          <Habit key="index" />;
+        })}
+      </div>
+    </div>
+  );
 }
