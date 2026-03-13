19bcab693cae3a82b5d2084ec5122af9b4bbe684
jckstllrd <jackstallard17@gmail.com>
2026-03-13 21:16:51 +0000

    refactor: finish off refactor:

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,11 @@
 import Dashboard from "./routes/Dashboard.jsx";
-import AddHabit from "./routes/AddHabit.jsx";
-import HabitDetail from "./routes/HabitDetail.jsx";
-import Settings from "./routes/Settings.jsx";
 import App from "./App.jsx";
 
 const routes = [
   {
     path: "/",
     element: <App />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "add", element: <AddHabit /> },
-      { path: "habit/:id", element: <HabitDetail /> },
-      { path: "settings", element: <Settings /> },
-    ],
+    children: [{ index: true, element: <Dashboard /> }],
   },
 ];
 
