b19d9bf4857e9cad581c8cb0dbe9267d6566627e
jckstllrd <jackstallard17@gmail.com>
2026-03-14 22:18:57 +0000

    style: add habit cell responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 import "./styles/App.css";
 import Habit from "./components/Habit";
-export default function App() {
-  const [habits, setHabits] = useState([]);
+import HabitForm from "./components/HabitForm";
 
+export default function App() {
+  const [habits, setHabits] = useState([
+    {
+      id: 0,
+      name: "running",
+      log_completed: {},
+    },
+  ]);
   function addHabit(habit) {
     setHabits((prev) => [...prev, habit]);
   }
+
   return (
     <>
       <div className="app-container">
         {habits.map((habit) => {
-          return <Habit key={habit.id} />;
+          return <Habit key={habit.id} habit={habit} />;
         })}
+        <HabitForm addHabit={addHabit} />
       </div>
     </>
   );
--- src/components/Habit.jsx
+++ src/components/Habit.jsx
@@ -1,3 +1,13 @@
-export default function Habit() {
-  return <div className="habit-cell"></div>;
+export default function Habit({ habit }) {
+  return (
+    <div className="habit-cell">
+      <div className="habit-content">
+        <h3 className="habit-name">{habit.name}</h3>
+        <div className="habit-completion">
+          <div className="habit-chart"></div>
+          <p className="habitStreak">Streak: 0</p>
+        </div>
+      </div>
+    </div>
+  );
 }
