cee290cd375d200d16b39a12ac3d97a6bf528ab2
jckstllrd <jackstallard17@gmail.com>
2026-03-14 17:15:55 +0000

    refactor: start from fresh learning concepts from scratch

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router";
-import routes from "./routes.jsx";
-
-const router = createBrowserRouter(routes);
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
