a4ea3c5a05c64355f21f7247f18c3605226bc539
jckstllrd <jackstallard17@gmail.com>
2026-04-15 19:31:10 +0100

    feat: add updateUser query to queries

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -15,11 +15,21 @@ const createUser = async (req, res) {
 
   try {
     const results = await pool.query("INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *", [username, email])
-
     res.status(201).send(`User added with ID: ${results.rows[0].id}`)
-
   } catch (err) {
     throw err;
+  }
+}
+
+const updateUser = async (req, res) {
+  const id = parseInd(req.params.id, 10)
+  const { username, email } = req.body
+
+  try {
+    await pool.query('UPDATE users SET username = $1, email = $2, id = $3', [username, email, id])
+    response.status(200).send(`User modified with ID: ${id}`)
+  } catch (err) {
+    throw err
   }
 }
 
