24d6d12a66fab0841a79b96656a784d29dc51514
jckstllrd <jackstallard17@gmail.com>
2026-04-15 19:25:28 +0100

    feat: add createUser query to queries

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -10,6 +10,18 @@ const getAllUsers = async (req, res){
   return rows;
 }
 
+const createUser = async (req, res) {
+  const { username, email } = req.body
+
+  try {
+    const results = await pool.query("INSERT INTO users (username, email) VALUES ($1, $2) RETURNING *", [username, email])
+
+    res.status(201).send(`User added with ID: ${results.rows[0].id}`)
+
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function getAllHabits() {
   const { rows } = await pool.query("SELECT * FROM habits");
