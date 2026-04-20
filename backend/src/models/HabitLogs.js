f91a214cf340ca52b616580a1d29158172754ab0
jckstllrd <jackstallard17@gmail.com>
2026-04-20 12:16:45 +0100

    feat: add habitLogs model for habit_logs table querying

---
+++ backend/src/models/HabitLogs.js
@@ -0,0 +1,73 @@
+import { pool } from "../config/db.js";
+
+const getAllHabitLogs = async () => {
+  try {
+    const results = await pool.query("SELECT * FROM habit_logs");
+    return results.rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getHabitLogById = async (id) => {
+  try {
+    const results = await pool.query("SELECT * FROM habit_logs WHERE id = $1", [
+      id,
+    ]);
+    return results.rows;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getAllHabitLogsByHabitId = async (habitId) => {
+  try {
+    const results = await pool.query(
+      "SELECT * FROM habit_logs WHERE habit_id = $1",
+      [habitId],
+    );
+    return results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createHabitLog = async (habit_id, logged_on) => {
+  try {
+    const results = await pool.query(
+      "INSERT INTO habit_logs (habit_id, logged_on) VALUES ($1, $2) RETURNING *",
+      [habit_id, logged_on],
+    );
+    return results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateHabitLog = async (id, logged_on, habit_id) => {
+  try {
+    await pool.query(
+      "UPDATE habit_logs SET logged_on = $1, habit_id = $2 WHERE id = $3",
+      [logged_on, habit_id, id],
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteHabitLog = async (id) => {
+  try {
+    await pool.query("DELETE FROM habit_logs WHERE id = $1", [id]);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export {
+  getAllHabitLogs,
+  getAllHabitLogsByHabitId,
+  getHabitLogById,
+  createHabitLog,
+  updateHabitLog,
+  deleteHabitLog,
+};
