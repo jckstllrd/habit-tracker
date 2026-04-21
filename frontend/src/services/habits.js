05fcffa1667d5320d8a2da42806aae6ba7a80856
jckstllrd <jackstallard17@gmail.com>
2026-04-21 19:49:53 +0100

    feat: add main functions for http requests in service layer for api

--- frontend/src/services/habits.js
+++ frontend/src/services/habits.js
@@ -1,3 +1,19 @@
 const basepath = "http://localhost:8080/habits";
 
+const userId = 1;
+
 export const getAllHabits = () => fetch(basepath).then((data) => data.json());
+
+export const getAllHabitsByUser = () =>
+  fetch(`${basepath}/user/${userId}`).then((data) => data.json());
+
+export const createHabit = (name) =>
+  fetch(basepath, {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  });
+
+export const deleteHabit = (habitId) =>
+  fetch(`${basepath}/${habitId}`, {
+    method: "DELETE",
+  });
