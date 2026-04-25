55068358a0be7b6a8fc6decaf4993a102fb5d04c
jckstllrd <jackstallard17@gmail.com>
2026-04-25 13:00:19 +0100

    feat: add getHabitCurrentStreak to call api endpoint with habitId

--- frontend/src/services/habitLogs.js
+++ frontend/src/services/habitLogs.js
@@ -3,6 +3,9 @@ const basepath = "http://localhost:8080/logs";
 export const getAllHabitLogs = () =>
   fetch(basepath).then((data) => data.json());
 
+export const getHabitCurrentStreak = (habit_id) =>
+  fetch(`${basepath}/habit/streak/${habit_id}`).then((data) => data.json());
+
 export const createHabitLog = (habit_id, logged_on) =>
   fetch(basepath, {
     method: "POST",
