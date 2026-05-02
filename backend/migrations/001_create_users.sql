6bb88354e46e74c27e47818a935bdd0d5fe351e3
jckstllrd <jackstallard17@gmail.com>
2026-05-02 13:08:17 +0100

    chore: untrack frontend .env

--- backend/migrations/001_create_users.sql
+++ backend/migrations/001_create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  username varchar(30),
-  email varchar(30),
-  password_hash char(60),
+  id UUID PRIMARY KEY gen_uuid(),
+  username varchar(50) UNIQUE NOT NULL,
+  email varchar(50) UNIQUE NOT NULL,
+  password_hash varchar(255) NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
