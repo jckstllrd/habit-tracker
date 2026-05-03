3de08ffac2b1a6f3915fcbb4bee72d36ea3714d9
jckstllrd <jackstallard17@gmail.com>
2026-05-03 16:35:11 +0100

    feat: add basic auth router and controller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import LocalStrategy from "passport-local";
 import usersRouter from "./src/routes/usersRouter.js";
 import habitsRouter from "./src/routes/habitsRouter.js";
 import habitLogsRouter from "./src/routes/habitLogsRouter.js";
+import authRouter from "./src/routes/authRouter.js";
 const app = express();
 
 app.use(cors());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -16,6 +23,7 @@ app.use(
 app.use("/users", usersRouter);
 app.use("/habits", habitsRouter);
 app.use("/logs", habitLogsRouter);
+app.use("/auth", authRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,7 @@
+export const loginUser = async (req, res, next) => {
+  console.log("logging in");
+};
+
+export const registerUser = async (req, res, next) => {
+  console.log("registering user");
+};
