b7208364ed2901eb5da05c3bf4e25321e90b1e93
jckstllrd <jackstallard17@gmail.com>
2026-05-04 19:05:55 +0100

    fix: update id to use uuid_generate_v4() for more secure identifiers

--- backend/migrations/001_create_users.sql
+++ backend/migrations/001_create_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY gen_uuid(),
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   username varchar(50) UNIQUE NOT NULL,
   email varchar(50) UNIQUE NOT NULL,
   password_hash varchar(255) NOT NULL,
