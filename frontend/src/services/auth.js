1ea1b35c59d99cb819a32f992668c87d20bd346a
jckstllrd <jackstallard17@gmail.com>
2026-05-09 22:08:52 +0100

    fix: update inputs from username to an email

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,18 +1,18 @@
 const basepath = `${import.meta.env.VITE_API_URL}/auth`;
 
-export const login = (username, password) =>
+export const login = (email, password) =>
   fetch(`${basepath}/login`, {
     method: "POST",
-    body: JSON.stringify({ username, password }),
+    body: JSON.stringify({ email, password }),
     headers: {
       "content-type": "application/json",
     },
   });
 
-export const register = (username, password, confirmPassword) =>
+export const register = (email, password, confirmPassword) =>
   fetch(`${basepath}/registerj`, {
     method: "POST",
-    body: JSON.stringify({ username, password, confirmPassword }),
+    body: JSON.stringify({ email, password, confirmPassword }),
     headers: {
       "content-type": "application/json",
     },
