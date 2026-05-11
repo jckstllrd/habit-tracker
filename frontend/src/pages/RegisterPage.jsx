822e175881daa128e4b20c06ad55df3e1fdc5c1f
jckstllrd <jackstallard17@gmail.com>
2026-05-11 18:36:42 +0100

    feat: implement register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,18 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
+import * as authService from "../services/auth.js";
+
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  let navigate = useNavigate();
+  let auth = useAuth();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    authService
+      .register(email, password, confirmPassword)
+      .then((res) => res.json())
+      .then((res) => {
+        auth.login(res.token);
+
+        navigate("/");
+      });
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">
-          Username: <input name="email" required type="text"></input>
+          Email:{" "}
+          <input
+            name="email"
+            onChange={(event) => setEmail(event.target.value)}
+            required
+            type="text"
+            value={email}
+          ></input>
         </label>
         <label htmlFor="password">
-          Password:<input required name="password" type="password"></input>
+          Password:
+          <input
+            required
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+            value={password}
+            type="password"
+          ></input>
         </label>
         <label htmlFor="confirmPassword">
           Confirm Password:
-          <input required name="confirmPassword" type="password"></input>
+          <input
+            required
+            name="confirmPassword"
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            value={confirmPassword}
+            type="password"
+          ></input>
         </label>
-
         <button type="submit">Login</button>
       </form>
     </>
