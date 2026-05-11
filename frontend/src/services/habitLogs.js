bff97e4c167edde59e491e4fd768cfc18888df81
jckstllrd <jackstallard17@gmail.com>
2026-05-11 18:02:36 +0100

    feat: add authorization header to every request

--- frontend/src/services/habitLogs.js
+++ frontend/src/services/habitLogs.js
@@ -1,21 +1,34 @@
 const basepath = `${import.meta.env.VITE_API_URL}/logs`;
 
-export const getAllHabitLogs = () =>
-  fetch(basepath).then((data) => data.json());
-
-export const getHabitCurrentStreak = (habit_id) =>
-  fetch(`${basepath}/habit/streak/${habit_id}`).then((data) => data.json());
-
-export const createHabitLog = (habit_id, logged_on) =>
-  fetch(basepath, {
+export const getAllHabitLogs = async () => {
+  const token = localStorage.getItem("session");
+  return fetch(basepath, {
+    headers: { authorization: `Bearer ${token}` },
+  }).then((data) => data.json());
+};
+export const getHabitCurrentStreak = async (habit_id) => {
+  const token = localStorage.getItem("session");
+  return fetch(`${basepath}/habit/streak/${habit_id}`, {
+    headers: { authorization: `Bearer ${token}` },
+  }).then((data) => data.json());
+};
+export const createHabitLog = (habit_id, logged_on) => {
+  const token = localStorage.getItem("session");
+  return fetch(basepath, {
     method: "POST",
     body: JSON.stringify({ habit_id, logged_on }),
     headers: {
       "content-type": "application/json",
+      authorization: `Bearer ${token}`,
     },
   });
-
-export const deleteHabitLog = (id) =>
-  fetch(`${basepath}/${id}`, {
+};
+export const deleteHabitLog = (id) => {
+  const token = localStorage.getItem("session");
+  return fetch(`${basepath}/${id}`, {
     method: "DELETE",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
   });
+};
--- frontend/src/services/habits.js
+++ frontend/src/services/habits.js
@@ -1,17 +1,28 @@
 const basepath = `${import.meta.env.VITE_API_URL}/habits`;
 
-export const getAllHabits = () => fetch(basepath).then((data) => data.json());
-
-export const createHabit = (name) =>
-  fetch(basepath, {
+export const getAllHabits = async () => {
+  const token = localStorage.getItem("session");
+  return fetch(basepath, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  }).then((data) => data.json());
+};
+export const createHabit = async (name) => {
+  const token = localStorage.getItem("session");
+  return fetch(basepath, {
     method: "POST",
     body: JSON.stringify({ name }),
     headers: {
       "content-type": "application/json",
+      authorization: `Bearer ${token}`,
     },
   });
-
-export const deleteHabit = (habitId) =>
-  fetch(`${basepath}/${habitId}`, {
+};
+export const deleteHabit = (habitId) => {
+  const token = localStorage.getItem("session");
+  return fetch(`${basepath}/${habitId}`, {
     method: "DELETE",
+    headers: { authorization: `Bearer ${token}` },
   });
+};
