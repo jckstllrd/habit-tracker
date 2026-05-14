da4534027729ef15e481e44157a4037f9e362b46
jckstllrd <jackstallard17@gmail.com>
2026-05-14 20:42:05 +0100

    feat: add error handling to auth forms

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,43 +6,53 @@ import { useAuth } from "../hooks/useAuth.js";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   let navigate = useNavigate();
   let auth = useAuth();
   const handleSubmit = async (event) => {
     event.preventDefault();
-    authService
-      .login(email, password)
-      .then((res) => res.json())
-      .then((res) => {
-        auth.login(res.token);
-        navigate("/");
-      });
+    try {
+      const response = await authService.login(email, password);
+
+      if (!response.ok) {
+        throw new Error(`Invalid email or password`);
+      }
+      const result = await response.json();
+
+      auth.login(result.token);
+      navigate("/");
+    } catch (error) {
+      setError(error);
+    }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email:{" "}
-          <input
-            name="email"
-            onChange={(event) => setEmail(event.target.value)}
-            required
-            type="text"
-            value={email}
-          ></input>
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input
-            required
-            name="password"
-            onChange={(event) => setPassword(event.target.value)}
-            value={password}
-            type="password"
-          ></input>
-        </label>
-        <button type="submit">Login</button>
-      </form>
+      <div className="loginForm">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            Email:{" "}
+            <input
+              name="email"
+              onChange={(event) => setEmail(event.target.value)}
+              required
+              type="text"
+              value={email}
+            ></input>
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              required
+              name="password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+              type="password"
+            ></input>
+          </label>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      <div className="errorMessage">{error ? error.message : ""}</div>
     </>
   );
 }
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -7,54 +7,67 @@ export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState(null);
   let navigate = useNavigate();
   let auth = useAuth();
   const handleSubmit = async (event) => {
     event.preventDefault();
-    authService
-      .register(email, password, confirmPassword)
-      .then((res) => res.json())
-      .then((res) => {
-        auth.login(res.token);
+    try {
+      const response = await authService.register(
+        email,
+        password,
+        confirmPassword,
+      );
 
-        navigate("/");
-      });
+      if (!response.ok) {
+        throw new Error(`Invalid email or password`);
+      }
+      const result = await response.json();
+
+      auth.login(result.token);
+      navigate("/");
+    } catch (error) {
+      setError(error);
+    }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Email:{" "}
-          <input
-            name="email"
-            onChange={(event) => setEmail(event.target.value)}
-            required
-            type="text"
-            value={email}
-          ></input>
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input
-            required
-            name="password"
-            onChange={(event) => setPassword(event.target.value)}
-            value={password}
-            type="password"
-          ></input>
-        </label>
-        <label htmlFor="confirmPassword">
-          Confirm Password:
-          <input
-            required
-            name="confirmPassword"
-            onChange={(event) => setConfirmPassword(event.target.value)}
-            value={confirmPassword}
-            type="password"
-          ></input>
-        </label>
-        <button type="submit">Register</button>
-      </form>
+      <div className="registerForm">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">
+            Email:{" "}
+            <input
+              name="email"
+              onChange={(event) => setEmail(event.target.value)}
+              required
+              type="text"
+              value={email}
+            ></input>
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              required
+              name="password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={password}
+              type="password"
+            ></input>
+          </label>
+          <label htmlFor="confirmPassword">
+            Confirm Password:
+            <input
+              required
+              name="confirmPassword"
+              onChange={(event) => setConfirmPassword(event.target.value)}
+              value={confirmPassword}
+              type="password"
+            ></input>
+          </label>
+          <button type="submit">Register</button>
+        </form>
+      </div>
+      <div className="errorMessage">{error ? error.message : ""}</div>
     </>
   );
 }
